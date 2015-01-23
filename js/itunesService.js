a3f85b0a503326e784ef50d49ec5eee53fa6bc14
Chris Netz <cnetz89@gmail.com>
2015-01-22 17:09:41 -0700

    progress, still have error

--- js/itunesService.js
+++ js/itunesService.js
@@ -1,6 +1,6 @@
 var app = angular.module('itunes');
 
-app.service('itunesService', function($http, $q){
+app.service('itunesService', function($http, $q, $interval){
   //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
   //Also not that we're using a 'service' and not a 'factory' so all your method you want to call in your controller need to be on 'this'.
 
@@ -9,5 +9,43 @@ app.service('itunesService', function($http, $q){
   //Note that in the above line, artist is the parameter being passed in. 
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
-    //Code here
+
+
+this.songData = function(artist){
+var deferred = $q.defer();
+  $http({
+        method: 'JSONP',
+        url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+      }).then(function(info){
+      var arrSongData = [];
+      var results = info.data.results;
+      for (var i = 0; i < results.length; i++) {
+       
+        var songData = {
+        AlbumArt: results[i].artworkUrl100,
+        Artist: results[i].artistName,
+        Collection: results[i].collectionName,
+        CollectionPrice: results[i].collectionPrice,
+        Play: results[i].previewUrl,
+        Type: results[i].kind
+      };
+      arrSongData.push(songData);
+      };
+        deferred.resolve(results);
+      }, function(err){
+        deferred.reject(err);
+      })
+      
+
+      return deferred.promise;
+  };
+
+
 });
+
+
+
+    	
+
+
+    
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,6 @@
 var app = angular.module('itunes');
-
-app.controller('mainCtrl', function($scope, itunesService){
+  
+  app.controller('mainCtrl', function($scope, itunesService){    
   //This is setting up the default behavior of our ng-grid. The important thing to note is
   //the 'data' property. The value is 'songData'. That means ng-grid is looking for songData on $scope and is putting whatever songData is into the grid.
   //this means when you make your iTunes request, you'll need to get back the information, parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
@@ -25,16 +25,30 @@ app.controller('mainCtrl', function($scope, itunesService){
     //code here
 
 
+ 
+
+
   //Now write a function that will call the method on the itunesService that is responsible for getting the data from iTunes, whenever the user clicks the submit button
   //*remember, that method should be expecting an artist name. The artist name is coming from the input box on index.html, head over there and check if that input box is tied to any specific model we could use.
   //Also note that that method should be retuning a promise, so you could use .then in this function.
     
     //Code here
+$scope.getSongData = function (){
+    itunesService.songData($scope.artist).then(function(results){
+   
+   $scope.songData = results; 
+    console.log('results', results)
 
+  }, function(err){
+    console.log(err);
+  });
+};
 
   //Check that the above method is working by entering a name into the input field on your web app, and then console.log the result
 
     //Code here
+
+
 
 
   //If everything worked you should see a huge array of objects inside your console. That's great! But unfortunately that's not what ng-grid is expecting. What you need to do now
@@ -51,12 +65,21 @@ app.controller('mainCtrl', function($scope, itunesService){
 
     //Code here
 
+ 
+
 
   //Once you have that final data array, you simply need to put it on the scope (or more specifically on the scope as songData). Once you do this ($scope.songData = myFinalArray) then ng-grid will see that and populate the page.
 
     //Code here
+
 });
 
+// console.log('result', result);
+//   }, function(error) {
+//     console.log('error', error);
+//   }, function(notification) {
+//     console.log('notification', notification);
+//   });
 
 
 
